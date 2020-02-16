5129b088e9cc15df0c88400773a9031517ff163a
Daniel Sasser <danielbsasser@gmail.com>
2020-02-16 03:50:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,12 @@ def initialize(name)
 end
 
 def add_student(name, grade)
- @roster[grade] = []
- @roster[grade] << name
+  if @roster.include?(grade)
+    @roster[grade] << name
+  else
+    @roster[grade] = []
+    @roster[grade] << name
+  end
 end
 
 
